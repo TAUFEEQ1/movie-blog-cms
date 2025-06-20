89d8a63ea14587fe6410546a560c5f3737ca6e31
Nsamba Taufeeq <nsambataufeeq@gmail.com>
2025-06-20 07:28:05 +0300

    feat: add transfer configuration to enable remote transfers in server setup

--- config/server.ts
+++ config/server.ts
@@ -4,4 +4,9 @@ export default ({ env }) => ({
   app: {
     keys: env.array('APP_KEYS'),
   },
+  transfer: {
+    remote: {
+      enabled: true, // or simply remove this block to use the default
+    },
+  }
 });
